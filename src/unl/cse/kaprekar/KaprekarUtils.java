5f51615a1774217854bb3e6b8bccd0a3288872ee
Trevor Cutsor <trevor.cutsor@huskers.unl.edu>
2017-08-31 11:27:01 -0500

    Removed unnecessary ()

--- src/unl/cse/kaprekar/KaprekarUtils.java
+++ src/unl/cse/kaprekar/KaprekarUtils.java
@@ -30,12 +30,12 @@ public class KaprekarUtils {
 
 		//we use a long to accommodate larger squares
 		long square = math.pow(n , 2);
-		int numDigits = (int) Math.log10(n) + 1;
+		int numDigits = int Math.log10(n) + 1;
 		long modulus = 1;
 		long first, second;
 		for(int i=1; i<=numDigits; i++) {
 			modulus *= 10;
-			first = square / modlus;
+			first = square / modulus;
 			second = square % modulus;
 			if(first > 0 &&
 			   first + second = n) {
